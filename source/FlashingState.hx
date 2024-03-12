b800ed98757b986372c94c7cbf3ec14f29d968e9
VaiLogoCaramba <150534279+VaiLogoCaramba@users.noreply.github.com>
2024-03-12 15:44:34 -0300

    Update FlashingState.hx

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,10 +24,10 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
+			"Hola chicos, Sinix ayudó con este mod!\n
 			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
+			Press A to disable them now or go to Options Menu.\n
+			Press B to ignore this message.\n
 			You've been warned!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
